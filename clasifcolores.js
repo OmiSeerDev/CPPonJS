dd51df90176cf2ce998b817b1231f2577a3d4c75
OmiSeerDev <omarmtinajero@gmail.com>
2022-11-29 11:22:20 -0600

    Adding object of Primary colors

--- clasifcolores.js
+++ clasifcolores.js
@@ -1,1 +1,10 @@
-console.log("Hola culito"); 
+var colors = {
+    red:"red",
+    blue:"blue",
+    green:"green"
+}
+if(colors.hasOwnProperty(colors.red)==false){
+    console.log (" Not primary")
+} else {
+console.log("Primary");
+}

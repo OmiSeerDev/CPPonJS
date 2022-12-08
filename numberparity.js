503fe13aace70ebfbf15c52e45b22b744ccf2480
OmiSeerDev <omarmtinajero@gmail.com>
2022-12-07 19:15:57 -0600

    Adding script

--- numberparity.js
+++ numberparity.js
@@ -1,7 +1,11 @@
-let quantity = prompt("Insert a number");
+function numclassif () {
+    let quantity = prompt("Insert a number");
 
-if (quantity%2 == 0){
-    alert(`${quantity} is an even number!`);
-} else {
-    alert(`${quantity} is an odd number`);
+    if (quantity%2 == 0){
+        alert(`${quantity} is an even number!`);
+    } else {
+        alert(`${quantity} is an odd number`);
+    }
 }
+
+object.onclick = numclassif();
